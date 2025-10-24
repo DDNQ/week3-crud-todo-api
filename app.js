d6d27b865beb4ccb092ad12a40ca05df879cf999
DDNQ <ddnkquarcoo@st.knust.edu.gh>
2025-10-24 13:42:28 +0000

    week3-crud-todo-api

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ app.get('/todos', (req, res) => {
   res.status(200).json(todos); // Send array as JSON
 });
 
+
+
 // POST New – Create
 app.post('/todos', (req, res) => {
-  const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
+  if (!req.body.task)
+    return res.status(400).json({ error: 'Task is required' });
+
+  const newTodo = {
+    id: todos.length ? Math.max(...todos.map(t => t.id)) + 1 : 1,
+    task: req.body.task,
+    completed: !!req.body.completed,
+  }; // Auto-ID
   todos.push(newTodo);
   res.status(201).json(newTodo); // Echo back
 });
@@ -42,6 +51,16 @@ app.get('/todos/completed', (req, res) => {
   res.json(completed); // Custom Read!
 });
 
+app.get('/todos/active', (req, res) => {
+  const active = todos.filter(t => !t.completed)
+  res.status(200).json(active);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).json({ error: 'Todo not found' });
+  res.status(200).json(todo);
+});
 app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
